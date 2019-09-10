dfc164eed1de429ca70fc65334d9bc6ba922f8a0
chris sayen <crsayen@gmail.com>
2019-09-10 15:24:05 -0400

    basic working condition

--- automation.py
+++ automation.py
@@ -1,16 +1,4 @@
-import pyautogui, time
-from pyautogui import keyDown, keyUp, press
-import os, path
-
-pyautogui.FAILSAFE = True
-here = path.abspath(path.dirname(__file__))
-
-def click(image, timeout=None, double=False):
-    l = None
-    start = time.time()
-    while l is None:
-        l = pyautogui.locateOnScreen(here + "/images/" + image)
-        elapsed = time.time() - startimport pyautogui, time
+import pyautogui, time, subprocess
 from pyautogui import keyDown, keyUp, press
 import socket, threading
 from threading import Thread
@@ -32,14 +20,16 @@ here = path.abspath(path.dirname(__file__))
 rcvd = ""
 
 def click(image, timeout=None, double=False, alt=False):
+    print("click({}, timeout={}, double={}, alt={})".format(image,timeout,double,alt))
     l = None
     start = time.time()
     while l is None:
-        l = pyautogui.locateOnScreen(here + "/images/" + image + ".PNG")
+        l = pyautogui.locateOnScreen(here + "\\images\\" + image + ".PNG")
         elapsed = time.time() - start
         if alt and l is None:
-            l = pyautogui.locateOnScreen(here + "/images/" + image + "_alt.PNG")
+            l = pyautogui.locateOnScreen(here + "\\images\\" + image + "_alt.PNG")
         if timeout is not None and elapsed >= timeout:
+            print("click: timeout: elapsed: {}".format(elapsed))
             break
     ret = l is not None
     if ret:
@@ -55,21 +45,44 @@ def rx(sock):
         time.sleep(0.2)
 
 
-def restart():
-    os.system("taskkill /f /im  JProg_7361.exe")
-    keyDown("winleft"); press("d"); keyUp("winleft")
-    click("showdesk", timeout=10, double=False)
-    click("folder", timeout=10, double=True)
-    click("exe", timeout=10, double=True, alt=True)
-    click("runanyway", timeout=10, double=False)
-    click("interface", timeout=10, double=False)
-    click("cleartoship", timeout=10, double=False)
+def restart(kill=True):
+    print("restarting")
+    if rcvd == "pause":
+            return
+    if kill:
+        os.system("taskkill /f /im  JProg_7361.exe")
+        if rcvd == "pause":
+            return
+    #keyDown("winleft"); press("d"); keyUp("winleft")
+    #if rcvd == "pause":
+     #       return
 
+    subprocess.Popen(["C:/Users/model/Desktop/7361-NJ-Nissan_Kiks/JProg_7361.exe"])
 
+    for t in [("interface", 10, False, True),("cleartoship", 10, False, False)]:
+        print("trying to click {}".format(t[0]))
+        if rcvd == "pause":
+            print("pause")
+            return
+        click(t[0], timeout=t[1], double=t[2], alt=t[3])
+    
 def main(sock):
     global rcvd
-    while 1: 
-        for i, step in enumerate([("start",10),("no",10),("proceed",10),("c_ok",20),("proceed",10),("e_ok",20)]):
+    restart(kill=False)
+    while 1:
+        while rcvd == "pause":
+            time.sleep(0.1)
+        for i, step in enumerate([
+            ("start",10, False),
+            ("no",10, False),
+            ("proceed",10, False),
+            ("c_ok",20, True),
+            ("proceed",10, False),
+            ("e_ok",20, False)
+        ]):
+            if rcvd == "pause":
+                break
+            print(step[0])
             if i == 2:
                 sock.sendto(bytes("proceed", "utf-8"), address)
                 for i in range(50):
@@ -77,45 +90,16 @@ def main(sock):
                         break
                     time.sleep(0.1)
                 rcvd = "" if rcvd == "ok" else "restart"
-            if rcvd == "restart" or not click(step[0], timeout=step[1]):
+            result = click(step[0], timeout=step[1], alt=step[2])
+            print("click: {}: result: {}".format(step[0], result))
+            if rcvd == "restart" or not result:
                 restart()
                 break
 
 if __name__ == "__main__":
-    address = ("192.168.7.2", 11005)
+    address = ("192.168.7.2", 11000)
     sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
-    sock.settimeout(5)
     sock.bind(("", 11000))
     rxt = Thread(target=rx,args=([sock]))
     rxt.start()
     main(sock)
-
-
-
-        if timeout is not None and elapsed >= timeout:
-            break
-    ret = l is not None
-    if ret:
-        for i in range(double + 1):
-            pyautogui.click(l[0] + 5, l[1] + 10)
-    return ret
-
-def restart():
-    os.system("taskkill /f /im  JProg_7361.exe")
-    keyDown("winleft"); press("d"); keyUp("winleft")
-    click("jprog_icon.PNG", timeout=10, double=True)
-    click("interface.PNG", timeout=10, double=True)
-    click("screw.PNG", timeout=10, double=True)
-    click("arrow.PNG", timeout=10, double=True)
-    click("cleartoship.PNG", timeout=10, double=True)
-
-def main():
-    while 1: 
-        for step in [("start",10),("no",10),("proceed",10),("c_ok",20),("proceed",10),("e_ok",20)]:
-            if not click(step[0] + ".PNG", timeout=step[1]):
-                restart()
-
-if __name__ == "__main__":
-    main()
-
-
--- replaceSeed.py
+++ replaceSeed.py
@@ -2,10 +2,20 @@ import time
 import subprocess, datetime, socket
 import sys, threading, os
 import can
+import Adafruit_BBIO.GPIO as GPIO
+
+GPIO.setup("P9_23", GPIO.IN) # button
+GPIO.setup("P8_19", GPIO.OUT)  # green LED
+GPIO.setup("P8_13", GPIO.OUT)  # red LED
 
 lastCommand = "stop"
 EXCHANGE_TIMEOUT = 60
 newlog = -1
+run = True
+pressed = False
+kill = False
+button = "P9_23"
+path = "/var/lib/cloud9/crackerLogs/seedkeylog"
 
 def rxcmd(sock, cmd):
     try:
@@ -13,24 +23,56 @@ def rxcmd(sock, cmd):
     except Exception as e:
         print(e)
         return False
-    if data_bytes is None or data_bytes.decode("utf-8") != cmd:
-        print("error: didn't receive '{}' command in time".format(cmd))   
+    if data_bytes is None:
+        print("error: didn't receive '{}' command in time, restarting".format(cmd))   
         return False
+    datastr = data_bytes.decode("utf-8")
+    if datastr != cmd:
+        print("error: expected '{}' but got '{}'".format(cmd, datastr))
     sock.sendto(bytes("ok", "utf-8"), address)
     return True
 
-def main(sock):
-    global lastCommand
-    global newlog
-    while 1:
+def main(sock, newlog):
+    global lastCommand, run
+    while run:
         print("starting")
-        with open("/var/lib/cloud9/crackerLogs/seedkeylog", "r") as f:
-            if newlog == -1:
+        with open(path, "r") as f:
+            if newlog == 0:
                 for line in f:
-                    newlog += 1
+                    newlog = newlog + 1
+                    print("newLog: {}".format(newlog))
         while not rxcmd(sock, "proceed"): continue
-        with open("/var/lib/cloud9/crackerLogs/seedkeylog", "a+") as f:
-            for s in ["100","62C 01 40 00 00 00 00 00 00","13FFE060","10244060 00","102C6060 00 33 0B 00 96 87","103BC060 00","103D6060 01","10444060 00 00","10448060 00 00 00 00","1045C060 40","10774060 00","1084A060 05","10A04060 00","10ACE060 1E 00 00 00 00 00 00 00","103DA060 00","10812060 00","10B0A060 00 1E 04","13FFE060","1084A060 05","10244060 02","103D6060 01","10ACE060 1E 00 10 00 00 00 00 00","10B0A060 00 2B 04","10ACE060 2B 00 00 00 00 00 00 00","10B0A060 00 16 04","10B0A060 00 19 04","10B0A060 00 0E 04","13FFE060","13FFE060"]:
+        with open(path, "a+") as f:
+            for s in [
+                "100",
+                "62C 01 40 00 00 00 00 00 00",
+                "13FFE060",
+                "10244060 00",
+                "102C6060 00 33 0B 00 96 87",
+                "103BC060 00",
+                "103D6060 01",
+                "10444060 00 00",
+                "10448060 00 00 00 00",
+                "1045C060 40","10774060 00",
+                "1084A060 05",
+                "10A04060 00",
+                "10ACE060 1E 00 00 00 00 00 00 00",
+                "103DA060 00",
+                "10812060 00",
+                "10B0A060 00 1E 04",
+                "13FFE060",
+                "1084A060 05",
+                "10244060 02",
+                "103D6060 01",
+                "10ACE060 1E 00 10 00 00 00 00 00",
+                "10B0A060 00 2B 04",
+                "10ACE060 2B 00 00 00 00 00 00 00",
+                "10B0A060 00 16 04",
+                "10B0A060 00 19 04",
+                "10B0A060 00 0E 04",
+                "13FFE060",
+                "13FFE060"
+            ]:
                 c = s.split(" ")
                 s_id = '0' + c[0] if len(c[0]) % 2 !=0 else c[0]
                 s_data = "".join(c[1:]) if len(c) != 1 else None
@@ -42,17 +84,21 @@ def main(sock):
                     channel="can0",
                 )
                 bus.send(msg)
-            line = doExchange()
-            if line is not None:
-                f.write(line)
+            result = doExchange(newlog)
+            if result[0]:
+                f.write(result[1])
+                newlog = newlog + 1
+            else:
+                print(result[1])
+    sock.sendto(bytes("pause", "utf-8"), address)
+    return
 
-def doExchange():
-    global lastCommand
-    global newlog
-    noSeed = True
+def doExchange(newlog):
+    global lastCommand, run
     i = 0
     start = time.time()
-    while noSeed:
+    while run:
+        data = None
         i+=1
         try:
             msg = bus.recv(0.5)
@@ -60,10 +106,12 @@ def doExchange():
             continue
         if msg is None:
             continue
+        print("received: {}".format(msg.data.hex()))
         if msg.data.hex() == '0227010000000000':
             seed = hex(newlog)[2:]
             while len(seed) < 4:
                 seed = '0' + seed
+            print("seed: {}".format(seed))
             data = '046701{}aaaaaa'.format(seed)
         elif msg.data.hex() == 'fe01280000000000':
             data = '0168aaaaaaaaaaaa'
@@ -82,22 +130,44 @@ def doExchange():
             data = '22323333313531AA'
         elif msg.data.hex()[:6] == "042702":
             newlog +=1
-            return "\n{} {}".format(seed,msg.data.hex()[6:10])
-        msg = can.Message(
-            arbitration_id=0x64c,
-            extended_id=False,
-            data=bytes.fromhex(data),
-            channel="can0",
-        )
-        bus.send(msg)
+            return (True, "\n{} {}".format(seed,msg.data.hex()[6:10]))
+        if data is not None:
+            print("sending: {}".format(data))
+            msg = can.Message(
+                arbitration_id=0x64c,
+                extended_id=False,
+                data=bytes.fromhex(data),
+                channel="can0",
+            )
+            bus.send(msg)
         if time.time() - start > EXCHANGE_TIMEOUT:
-            return None
- 
+            return (False, "error: doExchange: timeout")
+    return (False, "doExchange: pause")
+    
+
+def handlebutton(caller):
+    global run, kill, pressed
+    if pressed:
+        return
+    pressed = True
+    time.sleep(0.5)
+    pressed = False
+    GPIO.output("P8_19", GPIO.LOW)
+    GPIO.output("P8_13", GPIO.HIGH)
+    run = not run
+
 if __name__ == "__main__":
+    newlog = 0
     bus = can.interface.Bus(bustype="socketcan", channel="can0")
-    path = "/var/lib/cloud9/crackerLogs/"
     address = ("0.0.0.0", 11000)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(60)
     sock.bind(address)
-    main(sock)
+    GPIO.add_event_detect(button,GPIO.RISING,callback=handlebutton)
+    while 1:
+        main(sock, newlog)
+        while not run: 
+            time.sleep(0.5)
+        GPIO.output("P8_19", GPIO.HIGH)
+        GPIO.output("P8_13", GPIO.LOW)
+        sock.sendto(bytes("resume", "utf-8"), address)
